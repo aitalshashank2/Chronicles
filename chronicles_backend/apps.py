69c34693a0e7acc8466b7cfa6372e6ad986bdfc3
Shashank Aital <aitalshashank2@gmail.com>
2020-04-29 18:13:36 +0530

    Initialized Django Project and created an app. Made models.

---
+++ chronicles_backend/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChroniclesBackendConfig(AppConfig):
+    name = 'chronicles_backend'
---
+++ chronicles_backend/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class ChronicleUser(models.Model):
+    name = models.CharField(max_length=100)
+    isAdmin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    creator = models.ForeignKey(ChronicleUser, null=True, on_delete=models.SET_NULL, related_name='created_projects')
+    team = models.ManyToManyField(ChronicleUser, related_name='projects')
+    creation = models.DateTimeField('Timestamp of project creation')
+
+    def __str__(self):
+        return self.name
+
+
+class BugReport(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(ChronicleUser, null=True, on_delete=models.SET_NULL, related_name='reported_bugs')
+    heading = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+    person_in_charge = models.ForeignKey(ChronicleUser, null=True, on_delete=models.SET_NULL,
+                                         related_name='bugs_assigned')
+    creation = models.DateTimeField('Timestamp of bug report')
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-status', '-creation']
+
+    def __str__(self):
+        return f"{self.project}::{self.heading}"
+
+
+class Comment(models.Model):
+    report = models.ForeignKey(BugReport, on_delete=models.CASCADE)
+    creation = models.DateTimeField('Timestamp of comment')
+    commenter = models.ForeignKey(ChronicleUser, null=True, on_delete=models.SET_NULL)
+    body = models.CharField(max_length=1000)
+
+    class Meta:
+        ordering = ['-creation']
+
+    def __str__(self):
+        return f"{self.report.project}::{self.report}::{self.body}"
